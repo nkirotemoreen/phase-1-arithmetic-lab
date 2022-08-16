2b7040311ddb8b3b1494fb308c8dadeb9aeaa211
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-16 08:33:59 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 
-// Write your code here
+// Write your code
+/*--
+const multiply = function multiply(num1 ,num2) {
+
+    let num1 = 31;
+    let num2 = 2;
+    if ( typeof  num1 === 'number' && typeof num2 === 'number'
+    && num1 * num2 === 62) {
+        result = multiply(num1, num2)
+        
+    }
+    return result
+    
+}
+//mythoughts
+function multiply(num1, num2) {
+    let equation
+    if ( typeof  num1 === 'number' && typeof num2 === 'number'
+    && num1 * num2 === 62){
+       equation = num1 * num2 === 62
+
+    }
+
+}
+
+--*/
+//random numbers
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+//random
+let random = Math.floor(Math.random() * 10) + 1;
+//mod
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4;
+//highest number is a set
+let max = Math.max = (5, 10, 15, 20);
